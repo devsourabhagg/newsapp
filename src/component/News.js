665828d4856818a5b03d64fabb4ca0b5add58ef2
Sourabh Aggarwal <devsourabhagg@gmail.com>
2024-04-01 23:39:40 +0530

    badge added

--- src/component/News.js
+++ src/component/News.js
@@ -102,7 +102,9 @@ export default class News extends Component {
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <Newsitems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} urlPath={element.url} author={element.author?element.author:"unknown"} publishedDate={element.publishedAt}/>
+                            <Newsitems title={element.title ? element.title : ""} description={element.description ? element.description : ""} 
+                            imageUrl={element.urlToImage} urlPath={element.url} author={element.author?element.author:"unknown"} 
+                            publishedDate={element.publishedAt} source={element.source.name}/>
                         </div>
                     })}
                 </div>
--- src/component/Newsitems.js
+++ src/component/Newsitems.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 export default class Newsitems extends Component {
     render() {
-        let { title, descripiton, imageUrl, urlPath,author,publishedDate } = this.props; // this is called destructuring here this.props is an object from which respective values will be fetched
+        let { title, descripiton, imageUrl, urlPath, author, publishedDate,source } = this.props; // this is called destructuring here this.props is an object from which respective values will be fetched
         return (
             <div className='my-3'>
                 <div className="card">
-                    <img src={imageUrl?imageUrl:'https://i.ytimg.com/vi/T9PxZzRnYc8/maxresdefault.jpg'} className="card-img-top" alt="..." />
+                <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source} </span>
+                    <img src={imageUrl ? imageUrl : 'https://i.ytimg.com/vi/T9PxZzRnYc8/maxresdefault.jpg'} className="card-img-top" alt="..." />
                     <div className="card-body">
+                        
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{descripiton}</p>
                         <p className="card-text"><small className="text-body-secondary">By {author} on {publishedDate}</small></p>
