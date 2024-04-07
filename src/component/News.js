3d19d57175112d437cd4218a4dcad49df30c6816
Sourabh Aggarwal <devsourabhagg@gmail.com>
2024-04-07 15:32:29 +0530

    function added for reusability

--- src/component/News.js
+++ src/component/News.js
@@ -17,17 +17,23 @@ export default class News extends Component {
         category : PropTypes.string
     }
 
-    constructor() {
-        super();
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    constructor(props) {
+        console.log('at second this runs');
+        super(props);
         this.state = {
             articles: [],
             loading: false,
-            page:0,
+            page:1,
         }
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewApp`
     }
 
-    async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=93365dd331034e5ebb10b7c69321838e&page=1&pageSize=${this.props.pageSize}`;
+    async updateNews(){
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=93365dd331034e5ebb10b7c69321838e&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
         try {
             let response = await fetch(url);
             this.setState({loading: true}) ; // Await the fetch call
@@ -37,7 +43,6 @@ export default class News extends Component {
             let data = await response.json(); // Await parsing the JSON response
             this.setState({
                 articles: data.articles,
-                totalResults: data.totalResults,
                 loading: false
             });
         } catch (error) {
@@ -45,59 +50,33 @@ export default class News extends Component {
             // Handle error state here if needed
             this.setState({ loading: false });
         }
+    }
+ 
+    async componentDidMount() {
+        console.log('at last this runs');
+        this.updateNews();
     }
 
     handlePreClick = async() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=93365dd331034e5ebb10b7c69321838e&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-        try {
-            let response = await fetch(url);
-            this.setState({loading: true}) ; // Await the fetch call
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
-            let data = await response.json(); // Await parsing the JSON response
-            this.setState({
-                page: this.state.page-1,
-                articles: data.articles,
-                loading: false
-            });
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-            // Handle error state here if needed
-            this.setState({ loading: false });
-        }
-
+        this.setState({
+            page : this.state.page -1
+        })
+        this.updateNews();
     }
 
     handleNextClick = async() => {
-        if(!(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=93365dd331034e5ebb10b7c69321838e&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-        try {
-            let response = await fetch(url);
-            this.setState({loading: true}) ; // Await the fetch call
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
-            let data = await response.json(); // Await parsing the JSON response
-            this.setState({
-                page: this.state.page+1,
-                articles: data.articles,
-                loading: false
-            });
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-            // Handle error state here if needed
-            this.setState({ loading: false });
-        }
-
-        }    
+        this.setState({
+            page : this.state.page +1
+        })
+        this.updateNews(); 
     }
 
     render() {
+        console.log('at first this runs');
         return (
             <div className="container my-3">
                 <h1 className="text-center"> </h1>
-                <h1>NewsApp -Top Headlines</h1>
+                <h1>NewsApp -Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner/>}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
--- src/component/Newsitems.js
+++ src/component/Newsitems.js
@@ -6,7 +6,7 @@ export default class Newsitems extends Component {
         return (
             <div className='my-3'>
                 <div className="card">
-                <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source} </span>
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source} </span>
                     <img src={imageUrl ? imageUrl : 'https://i.ytimg.com/vi/T9PxZzRnYc8/maxresdefault.jpg'} className="card-img-top" alt="..." />
                     <div className="card-body">
                         
