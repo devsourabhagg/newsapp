c529fa9dfd9b0764f71ad9695ac6096630ae3349
Sourabh Aggarwal <devsourabhagg@gmail.com>
2024-04-07 15:34:23 +0530

    minor issue fixed

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import News from './component/News';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  pageSize=20;
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Routes>
           {/* here we are using the key parameter for forcibyly remounitng the componennt */}
-          <Route path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} />
+          <Route path="/" element={<News key="general" pageSize={20} country="in" category="general" />} />
           <Route path="/business" element={<News key="business"pageSize={20} country="in" category="business" />} />
           <Route path="/entertainment" element={<News  key="entertainment" pageSize={20} country="in" category="entertainment" />} />
           <Route path="/health" element={<News  key="health" pageSize={20} country="in" category="health" />} />
